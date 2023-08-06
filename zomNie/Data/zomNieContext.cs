c4315ac952fe87a3735bc6b9d3162bf2dd6d4b73
DELL <DELL@DESKTOP-3SL6BQP>
2023-08-06 10:08:39 +0700

    Add project files.

---
+++ zomNie/Data/zomNieContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using zomNie.Models;
+
+namespace zomNie.Data
+{
+    public class zomNieContext : DbContext
+    {
+        public zomNieContext (DbContextOptions<zomNieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<zomNie.Models.Category> Category { get; set; } = default!;
+
+        public DbSet<zomNie.Models.Product>? Product { get; set; }
+
+        public DbSet<zomNie.Models.User>? User { get; set; }
+    }
+}
